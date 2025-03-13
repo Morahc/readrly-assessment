b07af72c1a7a3ba2c31260dc0f3473223f198701
Morah Stephen <Morahstephen872@gmail.com>
2025-03-13 18:45:19 +0100

    fix: fixed build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Faq from "./components/Faq";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Footer from "./components/layout/footer";
 import NewsLetter from "./components/NewsLetter";
 import Services from "./components/Services";
